ea66effbc4dab33f35e8f46b7ddc185936d35043
MuhammadUmer126 <mad.abdullahshahab47@gmail.com>
2025-04-05 12:51:31 +0500

    Added Missing Key Props

--- src/components/activity/index.js
+++ src/components/activity/index.js
@@ -25,17 +25,21 @@ function Activity({ getJobList, getJobApplications }) {
               {/* <TabsTrigger value={"applied"}>Applied</TabsTrigger>
               <TabsTrigger value={"selected"}>Selected</TabsTrigger>
               <TabsTrigger value={"rejected"}>Rejected</TabsTrigger> */}
-              {status.map((s) => {
-                return <TabsTrigger value={s}>{s}</TabsTrigger>;
+              {status.map((s, index) => {
+                return (
+                  <TabsTrigger key={index} value={s}>
+                    {s}
+                  </TabsTrigger>
+                );
               })}
             </TabsList>
           </div>
           <div className="pb-24 pt-6">
             <div className="container mx-auto p-0 space-y-8">
               <div className="flex flex-col gap-4">
-                {status.map((s) => {
+                {status.map((s, index) => {
                   return (
-                    <TabsContent value={s}>
+                    <TabsContent key={index} value={s}>
                       {getJobList
                         .filter(
                           (jobItem) =>
@@ -50,9 +54,10 @@ function Activity({ getJobList, getJobApplications }) {
                                 (finalItem) => jobItem?._id == finalItem.jobID
                               ) > -1
                         )
-                        .map((item) => {
+                        .map((item, index) => {
                           return (
                             <CommonCard
+                              key={index}
                               icon={<Rocket />}
                               title={item?.title}
                               description={item?.companyName}
